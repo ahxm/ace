96f9ba5e220ce6e568a8fcf2be0ad9db526c72a6
bjyfanzhen <anzhen@jd.com>
2016-05-05 23:13:19 +0800

    分页类

---
+++ src/main/java/com/ace/entity/Page.java
@@ -0,0 +1,29 @@
+package com.ace.entity;
+
+import com.ace.util.PageData;
+import com.ace.util.Tools;
+
+/**
+ * Created by anzhen on 2016/5/5.
+ */
+public class Page {
+
+    private int pageSize;
+
+    private int totalPage;
+
+    private int total;
+
+    private int currentPage;
+
+    private int currentResult;
+
+    private boolean entityOrField;
+
+    private String pageStr;
+
+    private PageData pd = new PageData();
+
+
+
+}
